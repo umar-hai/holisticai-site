975419b29c58d0449ba5be93eaa2bc05e6d4fcc5
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-12 18:27:41 +0100

    missing return in seo

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -23,21 +23,23 @@ export default function Seo(props) {
   const image = new URL(props.image || defaults.image, defaults.siteUrl);
   const url = new URL(props.path || "/", defaults.siteUrl);
 
-  <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <link rel="canonical" href={url} />
-    {image && <meta name="image" content={image} />}
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url} />
+      {image && <meta name="image" content={image} />}
 
-    <meta property="og:url" content={url} />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    {image && <meta name="og:image" content={image} />}
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {image && <meta name="og:image" content={image} />}
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    {image && <meta name="twitter:image" content={image} />}
-  </Helmet>;
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
+    </Helmet>
+  );
 }
