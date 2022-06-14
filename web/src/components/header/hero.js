d3b1e37298d8d7624d2574d3c1544fd677b6148a
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-14 14:34:17 +0100

    make hero responsive

--- web/src/components/header/hero.js
+++ web/src/components/header/hero.js
@@ -5,7 +5,7 @@ import CheckItem from "../check-item";
 
 export default function Hero() {
   return (
-    <header className="h-[760px] max-h-[616px] bg-base-purple relative">
+    <header className="h-[988px] max-h-[988px] lg:max-h-[616px] bg-base-purple relative">
       <div className="absolute right-0 top-[-70px]">
         <StaticImage
           src="../../images/hai-logo-bg-1.png"
@@ -13,38 +13,61 @@ export default function Hero() {
           placeholder="blurred"
         ></StaticImage>
       </div>
-      <div className="absolute left-0 top-[400px]">
+      <div className="absolute left-0 sm:bottom-0 lg:top-[400px]">
         <StaticImage
           src="../../images/hai-logo-bg-2.png"
           alt=""
           placeholder="blurred"
         ></StaticImage>
       </div>
-      <div className="a"></div>
-      <div className="container mx-auto px-28 pt-5 pb-20">
-        <Title classNames="text-white w-[624px]">
+      <div className="container mx-auto px-6 lg:px-8 xl:px-28 pt-5 pb-20">
+        <Title classNames="text-white w-auto lg:w-[624px]">
           <div>Global Leaders in AI Risk Management</div>
         </Title>
-        <div className="w-[509px] mt-10 text-white font-roboto">
-          <p>
-            <span className="text-inactive-base-purple">Holistic AI</span>{" "}
-            ensures that enterprises can adopt AI with confidence and assurance.
-            AI does not need to be a high-risk liability; Holistic AI guarantees
-            AI that is safe, ethical, and legal. We assess data, models, and
-            processes, at any level of maturity or scale.
-          </p>
-          <p className="mt-10">
-            No matter at which maturity stage, location or scale, <br />
-            Holistic AI has a solution for you.
-          </p>
-        </div>
-        <div className="mt-8 flex flex-col space-y-4">
-          <CheckItem text="Enterprise-ready SaaS Solution"></CheckItem>
-          <CheckItem text="World-first AI Auditors"></CheckItem>
-          <CheckItem text="Shaping AI policy worldwide"></CheckItem>
+        <div className="flex xl:block">
+          <div className="flex-1">
+            <div className="w-auto lg:w-[509px] mt-10 text-white font-roboto">
+              <p className="mb-10">
+                <span className="text-inactive-base-purple">Holistic AI</span>{" "}
+                ensures that enterprises can adopt AI with confidence and
+                assurance. AI does not need to be a high-risk liability;
+                Holistic AI guarantees AI that is safe, ethical, and legal. We
+                assess data, models, and processes, at any level of maturity or
+                scale.
+              </p>
+
+              <div className="lg:hidden">
+                <StaticImage
+                  src="../../images/hero-screen-sm.png"
+                  quality={100}
+                  alt=""
+                ></StaticImage>
+              </div>
+
+              <p className="mt-10">
+                No matter at which maturity stage, location or scale, <br />
+                Holistic AI has a solution for you.
+              </p>
+            </div>
+
+            <div className="mt-8 flex flex-col space-y-4">
+              <CheckItem text="Enterprise-ready SaaS Solution"></CheckItem>
+              <CheckItem text="World-first AI Auditors"></CheckItem>
+              <CheckItem text="Shaping AI policy worldwide"></CheckItem>
+            </div>
+          </div>
+          <div className="hidden lg:flex xl:hidden">
+            <div className="mt-auto mb-4">
+              <StaticImage
+                src="../../images/hero-screen-sm.png"
+                quality={100}
+                alt=""
+              ></StaticImage>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex absolute top-[-70px] right-0">
+      <div className="hidden xl:flex absolute top-[-70px] right-0">
         <StaticImage
           quality={100}
           style={{ margin: "auto" }}
--- web/src/title.js
+++ web/src/title.js
@@ -5,7 +5,7 @@ export default function Title({ children, classNames, dividerColor }) {
   return (
     <>
       <h1
-        className={`${classNames} font-bold text-[64px] leading-[65px] mb-10 w-100`}
+        className={`${classNames} font-bold text-5xl lg:text-[64px] lg:leading-[65px] mb-10 w-100`}
       >
         <div>{children}</div>
       </h1>
