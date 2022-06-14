cce7ec68302b2f2ff4fe5d0035ca49a29da86ed1
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-14 16:59:06 +0100

    make solving trust section responsive

--- web/src/components/home/solving-trust.js
+++ web/src/components/home/solving-trust.js
@@ -21,18 +21,42 @@ export default function SolvingTrust() {
           </div>
         </div>
 
-        <div className="mt-7 flex space-x-20 justify-center">
-          <MissionIconText
-            title="Embedding Ethics in AI"
-            text="By pursuing Foundational Research, we aim to embed in systems attitudes and practices that by-design make these systems compliant to certain standards, regulations or expectations"
-          >
-            <StaticImage
-              alt=""
-              quality={100}
-              src="../../images/embed-ethics.png"
-            ></StaticImage>
-          </MissionIconText>
-          <div className="h-[363px] flex items-end">
+        <div className="flex flex-col gap-6">
+          <div className="mt-7 flex flex-col items-center sm:gap-6 lg:flex-row lg:space-x-20 justify-center">
+            <MissionIconText
+              title="Embedding Ethics in AI"
+              text="By pursuing Foundational Research, we aim to embed in systems attitudes and practices that by-design make these systems compliant to certain standards, regulations or expectations"
+            >
+              <StaticImage
+                alt=""
+                quality={100}
+                src="../../images/embed-ethics.png"
+              ></StaticImage>
+            </MissionIconText>
+            <div className="hidden xl:flex h-[363px]  items-end">
+              <MissionIconText
+                title="Demonstrable Trust"
+                text="By pursuing Applied Research, our goal is to create new metrics, methods, and processes that can improve system verification and mitigation "
+              >
+                <StaticImage
+                  alt=""
+                  quality={100}
+                  src="../../images/trust.png"
+                ></StaticImage>
+              </MissionIconText>
+            </div>
+            <MissionIconText
+              title="Scaling Trusted Practices "
+              text="By pursuing Engineering Excellence, we aim to make all our methods, research and practices affordable and easy-to-use. This is part of our commitment to build a global ecosystem of trust "
+            >
+              <StaticImage
+                alt=""
+                quality={100}
+                src="../../images/scaling.png"
+              ></StaticImage>
+            </MissionIconText>
+          </div>
+          <div className="xl:hidden flex justify-center">
             <MissionIconText
               title="Demonstrable Trust"
               text="By pursuing Applied Research, our goal is to create new metrics, methods, and processes that can improve system verification and mitigation "
@@ -44,16 +68,6 @@ export default function SolvingTrust() {
               ></StaticImage>
             </MissionIconText>
           </div>
-          <MissionIconText
-            title="Scaling Trusted Practices "
-            text="By pursuing Engineering Excellence, we aim to make all our methods, research and practices affordable and easy-to-use. This is part of our commitment to build a global ecosystem of trust "
-          >
-            <StaticImage
-              alt=""
-              quality={100}
-              src="../../images/scaling.png"
-            ></StaticImage>
-          </MissionIconText>
         </div>
       </HaiContainer>
     </section>
