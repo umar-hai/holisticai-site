2af6b41f6a79708c8c8c7a6cae38270c5084128e
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-15 11:30:09 +0100

    center titles for small screen

--- web/src/title.js
+++ web/src/title.js
@@ -5,12 +5,14 @@ export default function Title({ children, classNames, dividerColor }) {
   return (
     <>
       <h1
-        className={`${classNames} font-bold text-5xl lg:text-[64px] lg:leading-[65px] mb-10 w-100`}
+        className={`${classNames} text-center lg:text-left font-bold text-5xl lg:text-[64px] lg:leading-[65px] mb-10 w-100`}
       >
         <div>{children}</div>
       </h1>
 
-      <Divider color={dividerColor}></Divider>
+      <div className="flex justify-center lg:block">
+        <Divider color={dividerColor}></Divider>
+      </div>
     </>
   );
 }
