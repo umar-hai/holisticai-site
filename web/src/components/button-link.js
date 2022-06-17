77f30dad8594e82863ade7118fcca1a89f0a9f5e
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-17 10:52:50 +0100

    move pointer-event-auto to ButtonLink group

--- web/src/components/button-link.js
+++ web/src/components/button-link.js
@@ -4,13 +4,13 @@ import * as React from "react";
 export default function ButtonLink({ link, text, groupClass }) {
   return (
     <div
-      className={`group h-[61px] flex items-center justify-center ${groupClass}`}
+      className={`pointer-events-auto group h-[61px] flex items-center justify-center ${groupClass}`}
     >
       <Link
         style={{
           transition: "padding 0.15s ease-out",
         }}
-        className="pointer-events-auto inline-block font-semibold text-[19px] m-auto bg-white py-4 rounded-lg text-base-blue px-8 group-hover:py-3 group-hover:px-6 hover:py-3 hover:px-6 shadow-hai"
+        className="inline-block font-semibold text-[19px] m-auto bg-white py-4 rounded-lg text-base-blue px-8 group-hover:py-3 group-hover:px-6 hover:py-3 hover:px-6 shadow-hai"
         to={link}
       >
         {text}
