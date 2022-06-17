94742da04257cca68b768d97caa33c682d0ead97
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-17 16:49:10 +0100

    add extra variables to tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -15,8 +15,10 @@ module.exports = {
         "inactive-blue": "#a8b9f0",
         "text-body": "#1b1e27",
         "text-secondary": "#8492a6",
+        "text-title": "#141e41",
         light: "#f4f5f8",
         dark: "#141e41",
+        stroke: "#dfdfea",
       },
       boxShadow: {
         hai: "0px 4px 16px rgba(0, 0, 0, 0.15)",
