15d0fd1a71fc8933174a11c92ad0aabfcd969b51
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-20 17:07:29 +0100

    add alt tag to static image in job location

--- web/src/components/careers/job-location.js
+++ web/src/components/careers/job-location.js
@@ -5,7 +5,7 @@ export default function JobLocation() {
   return (
     <div className="flex items-center space-x-3">
       <div className="flex">
-        <StaticImage src="../../images/pin.png"></StaticImage>
+        <StaticImage alt="" src="../../images/pin.png"></StaticImage>
       </div>
       <div className="font-manrope text-text-primary font-normal text-base">
         London /Remote
