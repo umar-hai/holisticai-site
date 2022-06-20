00235dfd8dab1d467d4e587b6500d4d203d4fc39
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-20 11:10:41 +0100

    set quality of hai-platform-bullet image to 100

--- web/src/components/home/hai-platform-bullet.js
+++ web/src/components/home/hai-platform-bullet.js
@@ -5,7 +5,7 @@ export default function HaiPlatformBullet({ textClass, children }) {
   return (
     <div className="w-[219px] flex flex-col text-center space-y-2">
       <div>
-        <StaticImage alt="" src="../../images/logo-symbol.png"></StaticImage>
+        <StaticImage alt="" src="../../images/logo-symbol.png" quality={100}></StaticImage>
       </div>
       <div className={`${textClass} font-roboto`}>{children}</div>
     </div>
