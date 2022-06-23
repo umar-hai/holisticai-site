78a680ce5daa1f0a96dfc385071ee5a7833e4579
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-23 12:22:52 +0100

    fix button link hover style

--- web/src/components/button-link.js
+++ web/src/components/button-link.js
@@ -7,10 +7,7 @@ export default function ButtonLink({ link, text, groupClass }) {
       className={`pointer-events-auto group h-[61px] flex items-center justify-center ${groupClass}`}
     >
       <Link
-        style={{
-          transition: "padding 0.15s ease-out",
-        }}
-        className="inline-block font-semibold text-[19px] m-auto bg-white py-4 rounded-lg text-base-blue px-8 group-hover:py-3 group-hover:px-6 hover:py-3 hover:px-6 shadow-hai"
+        className="transition-all duration-150 inline-block font-semibold text-[19px] m-auto bg-white py-4 rounded-lg text-base-blue px-8 group-hover:text-[17px] group-hover:py-[15px] group-hover:px-[31px] shadow-hai"
         to={link}
       >
         {text}
