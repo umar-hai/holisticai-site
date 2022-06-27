3e1375e6575406481cf65b6770ceb6ce374c2bd8
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-27 16:04:24 +0100

    set width for UseCaseCard

--- web/src/components/home/use-case-card.js
+++ web/src/components/home/use-case-card.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export default function UseCaseCard({ title, children }) {
   return (
-    <div className="w-auto lg:max-w-[597px] h-auto lg:h-[151px] shadow-hai p-6 rounded-2xl flex flex-col space-y-1 text-white bg-inactive-blue bg-opacity-10">
+    <div className="w-auto lg:max-w-[597px] lg:w-[597px] h-auto lg:h-[151px] shadow-hai p-6 rounded-2xl flex flex-col space-y-1 text-white bg-inactive-blue bg-opacity-10">
       <div className="font-bold text-[22px]">{title}</div>
       <div className="font-roboto">{children}</div>
     </div>
